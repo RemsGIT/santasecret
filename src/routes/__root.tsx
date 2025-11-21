6f3dc1e8938fd06fa52330648e7fc5f68f29ce47
Remy CASTRO <remycastro27@icloud.com>
2025-11-21 15:00:53 +0100

    Clean code

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -29,7 +29,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Santa Secret 2025',
       },
     ],
     links: [
